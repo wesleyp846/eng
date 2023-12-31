0eb6be28516a552b7630c84936286de94e925a33
wesleyp846 <aventureiro-msn@hotmail.com>
2023-12-31 18:15:03 -0300

    tabela passagem

--- passagem/admin.py
+++ passagem/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Passagem
 
-# Register your models here.
+admin.site.register(Passagem)
--- passagem/views.py
+++ passagem/views.py
@@ -5,4 +5,4 @@ def tabela(request):
     # Filtra o queryset pelos dias pares do mês 
     passagens = Passagem.objects.filter(data__in=[2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]) 
     # Passa o queryset para o template 
-        return render(request, “tabela.html”, {“passagens”: passagens})
+    return render(request, "tabelas.html", {"passagens": passagens})
